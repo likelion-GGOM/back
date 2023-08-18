6fa542fad55ef356412739cdf5a90a2f845b9b7c
LGAIN <heli1420@naver.com>
2023-08-18 22:32:17 +0900

    a

--- ggom/board/models.py
+++ ggom/board/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
 
-
-
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
