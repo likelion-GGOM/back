a7bdc481bfb81dee556411630099dbfaef96ffda
LGAIN <heli1420@naver.com>
2023-08-18 15:01:38 +0900

    feat:게시글 목록

--- ggom/board/admin.py
+++ ggom/board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'create_date']  # 목록에 표시될 필드
+    search_fields = ['title', 'content']               # 검색할 수 있는 필드
+    list_filter = ['create_date']            # 필터링할 수 있는 필드
+
+admin.site.register(Question, QuestionAdmin)
